824411e61e2e214e62d7dd390f57c6e1a7955aa6
Iwan Phillips <iwanthomphil@gmail.com>
2019-11-02 12:56:59 +0000

    Added Product and Preference Seeders.

--- database/factories/PreferenceFactory.php
+++ database/factories/PreferenceFactory.php
@@ -7,6 +7,17 @@ use Faker\Generator as Faker;
 
 $factory->define(preference::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function () {
+            return factory(App\User::class)->create() - id;
+        },
+        'energy_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'carbohydrate_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'protein_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'fat_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'fiber_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'salt_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'sugar_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'saturated_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'sodium_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
     ];
 });
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,17 @@ use Faker\Generator as Faker;
 
 $factory->define(product::class, function (Faker $faker) {
     return [
-        //
+        'barcode' => $faker->ean13,
+        'name' => $faker->sentence($nbWords=3),
+        'weight_g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=5000),
+        'energy_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
+        'carbohydrate_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
+        'protein_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
+        'fat_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
+        'fiber_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
+        'salt_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
+        'sugar_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
+        'saturated_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
+        'sodium_100g' => $faker->randomFloat($nbMaxDecimals = 2, $min= 0, $max=100),
     ];
 });
