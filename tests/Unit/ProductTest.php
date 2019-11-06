113feb822b907dd006439276da805beb228e6bb9
Iwan Phillips <iwanthomphil@gmail.com>
2019-11-06 08:58:32 +0000

    Added Product Unit Test.

---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Product;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+    /**
+     * @test
+     * Test that a product is able to be scanned.
+     *
+     * @return void
+     */
+    public function a_product_can_be_scanned()
+    {
+        $product = factory(Product::class)->create();
+        $this->assertInstanceOf(BelongsToMany::class, $product->scanned());
+    }
+
+    /**
+     * @test
+     * Test that a product can contain allergens.
+     *
+     * @return void
+     */
+    public function a_product_contains_allergens()
+    {
+        $product = factory(Product::class)->create();
+        $this->assertInstanceOf(BelongsToMany::class, $product->allergens());
+    }
+
+    /**
+     * @test
+     * Test that a product fits diets.
+     *
+     * @return void
+     */
+    public function a_product_fits_diets()
+    {
+        $product = factory(Product::class)->create();
+        $this->assertInstanceOf(BelongsToMany::class, $product->diets());
+    }
+}
