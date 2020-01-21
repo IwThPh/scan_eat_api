570809cf8ad3e4d82f4744c49b3bdee0f25dc913
Iwan Phillips <iwanthomphil@gmail.com>
2020-01-21 19:10:12 +0000

    Changed to reflect default values.

--- database/factories/PreferenceFactory.php
+++ database/factories/PreferenceFactory.php
@@ -8,14 +8,14 @@ use Faker\Generator as Faker;
 $factory->define(preference::class, function (Faker $faker) {
     return [
         'user_id' => factory(App\User::class),
-        'energy_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-        'carbohydrate_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-        'protein_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-        'fat_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-        'fiber_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-        'salt_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-        'sugar_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-        'saturated_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-        'sodium_max' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
+        'energy_max' => 2000,
+        'carbohydrate_max' => 325,
+        'protein_max' => 50,
+        'fat_max' => 70,
+        'fiber_max' => 30,
+        'salt_max' => 6,
+        'sugar_max' => 90,
+        'saturated_max' => 20,
+        'sodium_max' => 2.3,
     ];
 });
